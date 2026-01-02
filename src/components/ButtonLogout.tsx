c43360b76b9738e99bb052b5cdf20f6498add6d5
yuanthio <yuanthiovirly26.9a.tik@gmail.com>
2026-01-02 21:39:48 +0700

    fix errors

--- src/components/ButtonLogout.tsx
+++ src/components/ButtonLogout.tsx
@@ -7,15 +7,17 @@ import { ReactNode } from "react";
 interface LogoutButtonProps {
   className?: string;
   children?: ReactNode;
+  onLogout?: () => void;
 }
 
-export default function LogoutButton({ className = "", children }: LogoutButtonProps) {
+export default function LogoutButton({ className = "", children, onLogout }: LogoutButtonProps) {
   const supabase = createClient();
   const router = useRouter();
 
   const logout = async () => {
     await supabase.auth.signOut();
     router.push("/");
+    onLogout?.();
   };
 
   return (
